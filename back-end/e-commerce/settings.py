8aef7a6d695b1787c2c29974c2afe0e77e49d9fd
Mariam <mari.kifshidze@gmail.com>
2023-02-14 21:41:54 +0400

    Add user and store app in INSTALLED_APPS

--- back-end/e-commerce/settings.py
+++ back-end/e-commerce/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # --Custom Apps
+    'user.apps.UserConfig',
+    'store.apps.StoreConfig'
 ]
 
 MIDDLEWARE = [
