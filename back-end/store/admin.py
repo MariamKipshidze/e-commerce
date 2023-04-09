a53fc7a8c2bbb7274a2c29cb807583ca09ecfe74
Mariam <mari.kifshidze@gmail.com>
2023-04-09 15:42:45 +0400

    Register store app models in django admin

--- back-end/store/admin.py
+++ back-end/store/admin.py
@@ -3,3 +3,22 @@ from django.contrib import admin
 from store.models import Product
 from store.models import ProductCollection
 from store.models import Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title', 'price', 'category']
+    search_fields = ['name']
+    list_filter = ['category']
+
+
+@admin.register(ProductCollection)
+class ProductCollectionAdmin(admin.ModelAdmin):
+    list_display = ['title']
+    search_fields = ['name']
+    list_filter = ['category']
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'is_active']
