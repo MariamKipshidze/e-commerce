17d651001f5b26b6366759236d477930f42ce00e
Mariam <mari.kifshidze@gmail.com>
2023-04-13 11:34:56 +0400

    Register User model

--- back-end/user/admin.py
+++ back-end/user/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ['get_full_name', 'email', 'company_name']
+    search_fields = ['email']
